21bea00895ddf0faa753bf180d85215027fdebc1
Ramin Tavakoli <Ramin6tavakoli6@gmail.com>
2024-04-28 15:43:12 +0330

    Exercise4 added

---
+++ FPGA_EX4/Code/SPI_Protocol.vhd
@@ -0,0 +1,125 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_arith.ALL;
+use ieee.std_logic_unsigned.all;
+
+entity SPI_Protocol is
+port(
+		Clk, SPI_Clk: in std_logic;
+		Data_Ready, Miso_in, Miso: in std_logic;
+		Data_in: in std_logic_vector(7 downto 0);
+		Data_out: out std_logic_vector(7 downto 0);
+		CS, SCK, Mosi, Data_Out_Ready: out std_logic
+);
+end SPI_Protocol;
+
+architecture Behavioral of SPI_Protocol is
+
+type state is (idle,send,transceive);
+signal pr_state,nx_state: state:=idle;
+--signal bit_counter : integer range -1 to 7 := 7;  -- counter for bits sent and received
+
+signal Datain: std_logic_vector(7 downto 0);
+signal Dataout: std_logic_vector(7 downto 0);
+
+begin
+
+p1: process(clk)
+begin
+
+if(clk'event and clk='1') then
+	pr_state <= nx_state;
+	--Datain <= Data_in;
+end if; 
+end process;
+
+p2:process(Data_Ready, pr_state, Miso_in, miso, clk)
+	begin
+		case pr_state is
+		when idle =>
+			if (Data_Ready = '1' and Miso_in = '0') then
+				nx_state <= send;
+			elsif (Data_Ready = '1' and Miso_in = '1') then
+				nx_state <= transceive;
+			else 
+				nx_state <= idle;
+			end if;
+			CS <= '1'; 
+			--SCK <= '1';
+
+		when send =>
+			if (Data_Ready = '0') then
+				nx_state <= idle;
+			elsif (Miso_in = '1') then
+				nx_state <= transceive;
+			else 
+				nx_state <= send;
+			end if;
+			CS <= '0'; 
+			--SCK <= SPI_CLK;
+			
+		when transceive =>
+			if (Data_Ready = '0') then
+				nx_state <= idle;
+			elsif (Miso_in = '0') then
+				nx_state <= send;
+			else 
+				nx_state <= transceive;
+			end if;
+			CS <= '0';
+			--SCK <= SPI_CLK;
+		when others=>
+			nx_state<=idle;
+		end case;
+
+end process; 
+
+p3: process(SPI_CLK)
+variable bit_counter : integer range -1 to 7 := 7;  -- counter for bits sent and received
+	begin
+		case pr_state is
+		when idle =>
+			SCK <= '1';
+		when send =>
+			SCK <= SPI_CLK;
+		when transceive =>
+			SCK <= SPI_CLK;
+		when others=>
+			SCK <= SPI_CLK;
+		end case;	
+		if	(SPI_CLK'event and SPI_clk='1') then
+			if(pr_state = idle)then
+				--CS <= '1';
+				--SCK <= '1';
+				Mosi <= '1';
+				Data_Out_Ready <= '0';
+				Dataout <= X"FF";
+			elsif(pr_state = send)then
+				--SCK <= SPI_CLK;
+				--CS <= '0';
+				Mosi <= Data_in(bit_counter);
+				Dataout <= X"FF";
+				bit_counter := bit_counter - 1; -- Decrement bit_counter after shifting out each bit	
+				if( bit_counter = -1) then
+					bit_counter := 7;
+				end if;
+			elsif(pr_state = transceive) then
+				--SCK <= SPI_CLK;
+				Dataout(7 - bit_counter) <= Miso; 
+				Data_out <= "UUUUUUUU"; 
+				bit_counter := bit_counter - 1;
+				--CS <= '0';
+				if( bit_counter = -1 ) then
+					Data_Out_Ready <= '1';
+					Data_out <= Dataout;
+					bit_counter := 7;
+				else
+				Data_Out_Ready <= '0';
+				end if;
+			end if;
+	
+		end if;
+end process;
+end Behavioral;
+
+
---
+++ FPGA_EX4/Code/TB_SPI_Porotocol.vhd
@@ -0,0 +1,72 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY TB_SPI_Porotocol IS
+END TB_SPI_Porotocol;
+ 
+ARCHITECTURE behavior OF TB_SPI_Porotocol IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT SPI_Protocol
+    PORT(
+         Clk : IN  std_logic;
+         SPI_Clk : IN  std_logic;
+         Data_Ready : IN  std_logic;
+         Miso_in : IN  std_logic;
+         Miso : IN  std_logic;
+         Data_in : IN  std_logic_vector(7 downto 0);
+         Data_out : OUT  std_logic_vector(7 downto 0);
+         CS : OUT  std_logic;
+         SCK : OUT  std_logic;
+         Mosi : OUT  std_logic;
+         Data_Out_Ready : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal Clk : std_logic := '0';
+   signal SPI_Clk : std_logic := '0';
+   signal Data_Ready : std_logic := '0';
+   signal Miso_in : std_logic := '0';
+   signal Miso : std_logic := '0';
+   signal Data_in : std_logic_vector(7 downto 0) := (others => '0');
+
+ 	--Outputs
+   signal Data_out : std_logic_vector(7 downto 0);
+   signal CS : std_logic;
+   signal SCK : std_logic;
+   signal Mosi : std_logic;
+   signal Data_Out_Ready : std_logic;
+
+   -- Clock period definitions
+   --constant Clk_period : time := 10 ns;
+   --constant SPI_Clk_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: SPI_Protocol PORT MAP (
+          Clk => Clk,
+          SPI_Clk => SPI_Clk,
+          Data_Ready => Data_Ready,
+          Miso_in => Miso_in,
+          Miso => Miso,
+          Data_in => Data_in,
+          Data_out => Data_out,
+          CS => CS,
+          SCK => SCK,
+          Mosi => Mosi,
+          Data_Out_Ready => Data_Out_Ready
+        );
+ 
+CLK <= not CLK after 10 ns; 
+SPI_CLK <= not SPI_CLK after 15 ns;  
+Data_Ready <= '1' after 90 ns,'0' after 350 ns,'1' after 400 ns,'0' after 700 ns,'1' after 750 ns; 
+Miso_in <= '1' after 400 ns, '0' after 950 ns, '1' after 1100 ns; 
+Miso <= not Miso after 20 ns; 
+Data_in <= x"79";
+
+END;
